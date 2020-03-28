87920fb7c3bbe77a3b8e129e8e4739cd805ed9f7
Ranthonym <rahul.anthonym@gmail.com>
2020-03-27 20:09:52 -0400

    add netlify.toml file

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import "index.scss";
 
 import Application from "components/Application";
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(<Application />, document.getElementById("root"));
